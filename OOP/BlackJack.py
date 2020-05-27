5e068f409ebb7b15dd77db172fa98277b35efd59
mardzien <mardzien@gmail.com>
2020-05-27 23:00:54 +0200

    oop game

---
+++ OOP/BlackJack.py
@@ -0,0 +1,78 @@
+""":argIn this milestone project you will be creating a Complete BlackJack Card Game in Python.
+
+Here are the requirements:
+
+You need to create a simple text-based BlackJack game
+The game needs to have one player versus an automated dealer.
+The player can stand or hit.
+The player must be able to pick their betting amount.
+You need to keep track of the player's total money.
+You need to alert the player of wins, losses, or busts, etc...
+And most importantly:
+
+You must use OOP and classes in some portion of your game. You can not just use functions in your game.
+Use classes to help you define the Deck and the Player's hand. There are many right ways to do this, so explore it well!
+Feel free to expand this game. Try including multiple players. Try adding in Double-Down and card splits!
+Remember to you are free to use any resources you want and as always:"""
+
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
+          'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+playing = True
+
+
+class Card(object):
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f"{self.suit} of {self.rank}"
+
+
+class Deck(object):
+
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append((rank, suit))
+
+    def __str__(self):
+        return f"{self.deck}"
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        return self.deck.pop()
+
+
+class Hand(object):
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += values[card[0]]
+        if card[0] == 'Ace':
+            self.aces += 1
+
+    def adjust_for_aces(self):
+        if self.value > 21 and self.aces > 0:
+            self.value -= 10
+
+
+test_deck = Deck()
+print(test_deck)
+test_deck.shuffle()
+test_deck.shuffle()
+print(test_deck)
+card = Card(*test_deck.deal())
+print(card)
